9628a5e3c839dbd037f38674e31c77fccf504f0b
billurbektas <billur.bektas@gmail.com>
2024-06-08 09:24:20 +0200

    Taking out the second set of the PRCs and updating the Figure on the paper.

--- R/04_rates.R
+++ R/04_rates.R
@@ -34,24 +34,38 @@ tax =
                       group_by(change, log_year_0)%>%
                       summarize_at(vars(fit:lwr), mean)%>%
                       ungroup()))
+
 # Predictions for the figure
 pred =
   tax %>%
   dplyr::select(axis, treatment, pred)%>%
   unnest(pred)%>%
-  mutate(change = factor(change, levels = c("Distance to origin controls","Distance to destination controls")))%>%
-  mutate(treatment = case_when(treatment == "control"&change == "Distance to origin controls"~"Destination controls",
-                               treatment == "control"&change == "Distance to destination controls"~"Origin controls",
-                               treatment == "warmed"~"Warmed"))%>%
-  mutate(axis = recode(axis, RDA1 = "Canonical coefficient (PRC axis 1)",
-                       RDA2 = "Canonical coefficient (PRC axis 2)"))%>%
+  mutate(treatment = case_when(treatment == "control"&change == "Distance to origin controls"~"destControls",
+                               treatment == "control"&change == "Distance to destination controls"~"originControls",
+                               treatment == "warmed"~"warmed"))%>%
   mutate(experiment = "model")%>%
-  mutate(Year_0 = exp(log_year_0))
+  mutate(Year_0 = exp(log_year_0))%>%
+  mutate(change = ifelse(change == "Distance to destination controls"& treatment == "originControls", "Distance between controls",change))%>%
+  mutate(change = ifelse(change == "Distance to origin controls"& treatment == "destControls", "remove",change))%>%
+  filter(change !="remove")%>%
+  mutate(change = factor(change, levels = c("Distance between controls",
+                                            "Distance to origin controls",
+                                            "Distance to destination controls")))%>%
+  mutate(treatment = recode(treatment, destControls = "Destination controls",
+                            originControls = "Origin controls", 
+                            warmed = "Warmed"))%>%
+  mutate(axis = recode(axis, RDA1 = "Canonical coefficient (PRC axis 1)",
+                       RDA2 = "Canonical coefficient (PRC axis 2)"))
 
 ## Get Figures----
-pslope=
+pslope =
   slopes %>%
-  mutate(change = factor(change, levels = c("Distance to origin controls","Distance to destination controls")))%>%
+  mutate(change = ifelse(change == "Distance to destination controls"& treatment == "originControls", "Distance between controls",change))%>%
+  mutate(change = ifelse(change == "Distance to origin controls"& treatment == "destControls", "remove",change))%>%
+  filter(change !="remove")%>%
+  mutate(change = factor(change, levels = c("Distance between controls",
+                                            "Distance to origin controls",
+                                            "Distance to destination controls")))%>%
   mutate(treatment = recode(treatment, destControls = "Destination controls",
                             originControls = "Origin controls", 
                             warmed = "Warmed"))%>%
@@ -61,49 +75,63 @@ pslope=
   ggplot()+
   geom_hline(yintercept = 0, color = "black")+
   TP_theme()+
-  geom_point(alpha = 0.3, aes(Year_0, contrast, group = interaction(experiment, treatment), color = treatment))+
-  stat_smooth(aes(Year_0, contrast, group = interaction(experiment, treatment), color = treatment), geom = "line", method = "lm", formula = y~log(x), se = FALSE, linewidth = 0.5, alpha = 0.3)+
-  geom_line(data = pred, aes(Year_0, y= fit, group = interaction(experiment, treatment), color = treatment), linewidth = 1)+
-  geom_ribbon(data = pred, aes(Year_0, ymin = lwr, ymax = upr, group = interaction(experiment, treatment), fill = treatment), alpha = 0.1, show.legend = FALSE)+
-  facet_grid(axis~change)+
-  scale_color_manual(values = c("#ff7f00","#197af6","#ff0000"))+
-  scale_fill_manual(values = c("#ff7f00","#197af6","#ff0000"))+
+  geom_point(alpha = 0.3, aes(Year_0, contrast, group = interaction(experiment, change), color = change))+
+  stat_smooth(aes(Year_0, contrast, group = interaction(experiment, change), color = change, linetype = change), 
+              geom = "line", method = "lm", formula = y~log(x), se = FALSE, linewidth = 0.5, alpha = 0.3)+
+  geom_line(data = pred, aes(Year_0, y= fit, group = interaction(experiment, change), color = change, linetype = change), linewidth = 1)+
+  geom_ribbon(data = pred, aes(Year_0, ymin = lwr, ymax = upr, group = change, fill = change), alpha = 0.1, show.legend = FALSE)+
+  facet_grid(axis~., scales = "free_y")+
+  scale_color_manual(values = c("grey30","#ff0000","#ff0000"))+
+  scale_fill_manual(values = c("grey30","#ff0000","#ff0000"))+
   scale_x_continuous(breaks = seq(0,9,1))+
   scale_y_continuous(breaks = seq(0,0.7,0.1))+
-  labs(color = "", x= "Experimental years", y = "Taxonomic distance")
+  scale_linetype_manual(values = c("solid","solid","dotted"))+
+  labs(color = "", linetype = "", x= "Experimental years", y = "Taxonomic distance")
+pslope
 
 ptax=
   tax %>%
   dplyr::select(axis, treatment, cont)%>%
   unnest(cont)%>%
-  mutate(change = factor(change, levels = c("Distance to origin controls","Distance to destination controls")))%>%
-  mutate(treatment = case_when(treatment == "control"&change == "Distance to origin controls"~"Destination control",
-                               treatment == "control"&change == "Distance to destination controls"~"Origin control",
-                               treatment == "warmed"~"Warmed"))%>%
+  mutate(treatment = case_when(treatment == "control"&change == "Distance to origin controls"~"destControls",
+                               treatment == "control"&change == "Distance to destination controls"~"originControls",
+                               treatment == "warmed"~"warmed"))%>%
+  mutate(change = as.character(change))%>%
+  mutate(change = ifelse(change == "Distance to destination controls"& treatment == "originControls", "Distance between controls",change))%>%
+  mutate(change = ifelse(change == "Distance to origin controls"& treatment == "destControls", "remove",change))%>%
+  filter(change !="remove")%>%
+  mutate(change = factor(change, levels = c("Distance between controls",
+                                            "Distance to origin controls",
+                                            "Distance to destination controls")))%>%
+  mutate(treatment = recode(treatment, destControls = "Destination controls",
+                            originControls = "Origin controls", 
+                            warmed = "Warmed"))%>%
   mutate(axis = recode(axis, RDA1 = "Canonical coefficient (PRC axis 1)",
                        RDA2 = "Canonical coefficient (PRC axis 2)"))%>%
-  mutate(pval = factor(pval(p.value), levels = c("non-significant","*","***")))%>%
+  mutate(pval = factor(pval(p.value), levels = c("","*","***")))%>%
   
   
-  ggplot(aes(treatment, log_year_0.trend, color = treatment, alpha = pval))+
+  ggplot(aes(change, log_year_0.trend, color = change, label = pval))+
   geom_hline(yintercept = 0, color = "grey50")+
   TP_theme()+
   geom_point(size = 3)+
   geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), linewidth = 1)+
-  facet_grid(axis~change, scale = "free_x")+
-  scale_color_manual(values = c("#ff7f00","#197af6","#ff0000"))+
-  scale_alpha_manual(values = c(0.3,0.6,1))+
+  geom_text(aes(y = upper.CL+0.003))+
+  facet_grid(axis~., scale = "free_x")+
+  scale_color_manual(values = c("grey30","#ff0000","#ff0000"))+
+  scale_alpha_manual(values = c(0.2,0.5,1))+
   guides(color = "none")+
   scale_x_discrete(labels = label_wrap(10))+
-  theme(strip.text = ggplot2::element_text(size  = 12,  hjust = 0))+
-  labs(x = "", y = "Change in taxonomic distances over experimental years (slopes)", alpha= "Significant slopes")
+  theme(strip.text = ggplot2::element_text(size  = 16,  hjust = 0))+
+  labs(x = "", linetype = "", y = "Change in taxonomic distances over experimental years (slopes)", alpha= "Significant slopes")
+ptax
 
-pdf(here("plot","taxonomic_distances.pdf"), height = 9, width = 15)
-pp=ggarrange(pslope, ptax, widths = c(1.2, 0.9), common.legend = TRUE, legend = "bottom")
+pdf(here("plot","taxonomic_distances.pdf"), height = 10, width = 13)
+pp=ggarrange(pslope, ptax, widths = c(1.2, 0.9), common.legend = FALSE, legend = "bottom")
 print(pp)
 dev.off()
 
-## Get results table
+ ## Get results table
 res.tax =
   tax %>%
   dplyr::select(axis, treatment, cont, R2)%>%
@@ -112,3 +140,20 @@ res.tax =
   mutate_if(is.numeric, ~ ifelse(is.na(.), NA, round(., 3)))
 
 write.csv(res.tax, file = here ("output", "res.tax.csv")) 
+# Get results per experiment
+tax %>%
+  filter(treatment == "warmed")%>%
+  dplyr::select(treatment, axis, exp)%>%
+  unnest(exp)%>%
+  mutate(pval = ifelse(p.value<0.05, "s", "ns"))%>%
+  mutate(sign = factor(as.character(sign(log_year_0.trend))))%>%
+  group_by(axis, change, pval, sign)%>%
+  summarize(n = n())
+
+tax %>%
+  filter(treatment == "warmed")%>%
+  dplyr::select(treatment, axis, exp)%>%
+  unnest(exp)%>%
+  mutate(pval = ifelse(p.value<0.05, "s", "ns"))%>%
+  group_by(axis, change)%>%
+  summarize(max = max(log_year_0.trend), min = min(log_year_0.trend))
